333096ea4981721485840d406ad52c26eac2e888
Anderson Medeiros <anderson.santos.medeiros@hotmail.com>
2023-06-06 20:49:05 -0300

    Improved code typing and initial integration

--- src/calendar.ts
+++ src/calendar.ts
@@ -13,29 +13,18 @@ const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
 const TOKEN_PATH = path.join(process.cwd(), "token.json");
 const CREDENTIALS_PATH = path.join(process.cwd(), "credentials.json");
 
-/**
- * Reads previously authorized credentials from the save file.
- *
- * @return {Promise<OAuth2Client|null>}
- */
-async function loadSavedCredentialsIfExist() {
+function loadSavedCredentialsIfExist() {
   try {
-    const content = await fs.readFile(TOKEN_PATH);
+    const content = fs.readFileSync(TOKEN_PATH, "utf-8");
     const credentials = JSON.parse(content);
-    return google.auth.fromJSON(credentials);
+    return google.auth.fromJSON(credentials) as any;
   } catch (err) {
     return null;
   }
 }
 
-/**
- * Serializes credentials to a file compatible with GoogleAUth.fromJSON.
- *
- * @param {OAuth2Client} client
- * @return {Promise<void>}
- */
-async function saveCredentials(client) {
-  const content = await fs.readFile(CREDENTIALS_PATH);
+function saveCredentials(client: OAuth2Client) {
+  const content = fs.readFileSync(CREDENTIALS_PATH, "utf-8");
   const keys = JSON.parse(content);
   const key = keys.installed || keys.web;
   const payload = JSON.stringify({
@@ -44,51 +33,30 @@ async function saveCredentials(client) {
     client_secret: key.client_secret,
     refresh_token: client.credentials.refresh_token,
   });
-  await fs.writeFile(TOKEN_PATH, payload);
+  fs.writeFileSync(TOKEN_PATH, payload);
 }
 
-/**
- * Load or request or authorization to call APIs.
- *
- */
 async function authorize() {
-  let client = await loadSavedCredentialsIfExist();
-  if (client) {
-    return client;
+  const savedClient = loadSavedCredentialsIfExist();
+
+  if (savedClient) {
+    return savedClient;
   }
-  client = await authenticate({
+
+  const client = await authenticate({
     scopes: SCOPES,
     keyfilePath: CREDENTIALS_PATH,
   });
+
   if (client.credentials) {
-    await saveCredentials(client);
+    saveCredentials(client);
   }
+
   return client;
 }
 
-/**
- * Lists the next 10 events on the user's primary calendar.
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-async function listEvents(auth) {
-  const calendar = google.calendar({ version: "v3", auth });
-  const res = await calendar.events.list({
-    calendarId: "primary",
-    timeMin: new Date().toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: "startTime",
-  });
-  const events = res.data.items;
-  if (!events || events.length === 0) {
-    console.log("No upcoming events found.");
-    return;
-  }
-  console.log("Upcoming 10 events:");
-  events.map((event, i) => {
-    const start = event.start.dateTime || event.start.date;
-    console.log(`${start} - ${event.summary}`);
-  });
+export function subscribeToCalendar(listener: (auth: OAuth2Client) => void) {
+  authorize().then(listener).catch(console.error);
 }
 
-authorize().then(listEvents).catch(console.error);
+export type OAuth2Client = typeof google.auth.OAuth2.prototype;
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,81 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { google } from "googleapis";
+import { OAuth2Client, subscribeToCalendar } from "./calendar";
 
-export function activate() {
-  vscode.window.showInformationMessage('Hello World from Google Calendar!');
+export class GoogleCalendarProvider
+  implements vscode.TreeDataProvider<CalendarEvent>
+{
+  private calendarInstance: ReturnType<typeof google.calendar> | undefined;
+
+  constructor(private workspaceRoot: string) {
+    this.listenForEvents = this.listenForEvents.bind(this);
+    subscribeToCalendar(this.listenForEvents);
+  }
+
+  private async listenForEvents(auth: OAuth2Client) {
+    if (!this.calendarInstance) {
+      this.calendarInstance = google.calendar({ version: "v3", auth });
+    }
+
+    const response = await this.calendarInstance.events.list({
+      calendarId: "primary",
+      timeMin: new Date().toISOString(),
+      maxResults: 10,
+      singleEvents: true,
+      orderBy: "startTime",
+    });
+
+    const events = response.data.items;
+
+    if (!events || events.length === 0) {
+      return;
+    }
+
+    events.map((event) => {
+      const start = event.start?.dateTime || event.start?.date;
+      console.log(`${start} - ${event.summary}`);
+    });
+  }
+
+  getTreeItem(element: CalendarEvent): vscode.TreeItem {
+    return element;
+  }
+
+  getChildren(element?: CalendarEvent): Thenable<CalendarEvent[]> {
+    if (!this.workspaceRoot) {
+      vscode.window.showInformationMessage(
+        "No CalendarEvent in empty workspace"
+      );
+      return Promise.resolve([]);
+    }
+
+    return Promise.resolve([]);
+  }
 }
 
-export function deactivate() {}
+class CalendarEvent extends vscode.TreeItem {
+  constructor(
+    public readonly label: string,
+    private version: string,
+    public readonly collapsibleState: vscode.TreeItemCollapsibleState
+  ) {
+    super(label, collapsibleState);
+    this.tooltip = `${this.label}-${this.version}`;
+    this.description = this.version;
+  }
+
+  // iconPath = {
+  //   light: path.join(__filename, '..', '..', 'resources', 'light', 'CalendarEvent.svg'),
+  //   dark: path.join(__filename, '..', '..', 'resources', 'dark', 'CalendarEvent.svg')
+  // };
+}
+
+const rootPath =
+  vscode.workspace.workspaceFolders &&
+  vscode.workspace.workspaceFolders.length > 0
+    ? vscode.workspace.workspaceFolders[0].uri.fsPath
+    : undefined;
+
+vscode.window.createTreeView("google-calendar-view", {
+  treeDataProvider: new GoogleCalendarProvider(`${rootPath}`),
+});
